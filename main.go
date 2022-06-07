80827b973bf8bd60fe15a88c84cb28f3b2600fca
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-06-07 13:35:50 -0500

    project initializated

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	file, err := os.Open("sample.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Scanner := bufio.NewScanner(file)
+	Scanner.Split(bufio.ScanWords)
+
+	for Scanner.Scan() {
+		if strings.Contains(Scanner.Text(), "ng-version=") {
+			fmt.Println("Gotcha")
+			break
+		}
+
+		fmt.Println(Scanner.Text())
+	}
+
+	if err := Scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
