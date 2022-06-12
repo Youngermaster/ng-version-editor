b94ad2c448f35ed139a82b001580e56eeb308d7d
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-06-12 16:57:12 -0500

    A little bit of code added

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
+use ng_version_editor::Config;
 use regex::Regex;
+use std::env;
+use std::process;
 
+// ANCHOR: here
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = ng_version_editor::run(config) {
+        eprintln!("Application error: {e}");
+
+        process::exit(1);
+    }
     edit_version();
+    remove_tag();
 }
 
 fn edit_version() {
-    let re = Regex::new(r"(\d{4})-(\d{2})-(\d{2})").unwrap();
-    let text = "2012-03-14, 2013-01-01 and 2014-07-05";
-    for cap in re.captures_iter(text) {
-        println!("Month: {} Day: {} Year: {}", &cap[2], &cap[3], &cap[1]);
+    let parenthesis_regex = Regex::new(r#""(.*?)""#).unwrap();
+    let text = r#"dsaidajsdio "test 0", "test 1" stuf stuff"#;
+    for capture in parenthesis_regex.captures_iter(text) {
+        println!("Quoted text => {}", &capture[0]);
     }
 }
+
+fn remove_tag() {
+    println!("Removing tag...");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
+}
+
+// fn reading_file() {}
